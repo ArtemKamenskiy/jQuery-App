72de80e45bb9987f211affe7a41a3bcbc0a3d36c
Artem Kamenskyi <artemkamenskiy98@gmail.com>
2020-12-29 22:33:25 +0300

    #4 added data in modal, redesign page

--- functions/functions.js
+++ functions/functions.js
@@ -4,6 +4,9 @@ const popupUserInfo = document.querySelector(".popup__user-info");
 const blur = document.querySelector(".blur");
 const closeBtn = document.querySelector(".close__btn");
 const closeBtnSecond = document.querySelector(".close__btn-second");
+const userInfoMain = document.querySelector(".user-info__list-main");
+const address = document.querySelector('.user-info__list-address');
+const company = document.querySelector('.user-info__list-company');
 
 function blurOn() {
     jQuery(blur).css('display', 'flex');
@@ -33,6 +36,12 @@ function popupOn() {
     jQuery(popupUserInfo).css("display", "flex");
 }
 
+function clearPopupInfo() {
+    jQuery(userInfoMain).empty();
+    jQuery(address).empty();
+    jQuery(company).empty();
+}
+
 function popupClose() {
     overflowOff();
     jQuery(closeBtn).on('click', function () {
@@ -41,10 +50,11 @@ function popupClose() {
         overflowOn();
     })
     jQuery(closeBtnSecond).on('click', function () {
+        clearPopupInfo();
         popupUserOff();
         blurOff();
         overflowOn();
     })
 }
 
-export {blurOn, blurOff, overflowOn, overflowOff, popupOff, popupClose, popupOn, popupUserOff};
+export {blurOn, blurOff, overflowOn, overflowOff, popupOff, popupClose, popupOn, popupUserOff, clearPopupInfo};
--- script.js
+++ script.js
@@ -2,59 +2,114 @@ import {blurOn, popupClose, popupOn} from "./functions/functions.js";
 
 jQuery(document).ready(function () {
     const btnGetNames = document.querySelector('.btn_get-names');
-    const btnGetUsernames = document.querySelector('.btn_get-usernames');
-    const btnGetEmail = document.querySelector(".btn_get-email");
     const userName = document.querySelector(".user-name");
-    const userUsername = document.querySelector(".user-username");
-    const userEmail = document.querySelector(".user-email");
     const selectNames = document.querySelector(".select__names");
     const userInfoTitle = document.querySelector(".user-info__title");
+    const userInfoMain = document.querySelector(".user-info__list-main");
+    const address = document.querySelector('.user-info__list-address');
+    const company = document.querySelector('.user-info__list-company');
 
     jQuery.get('https://jsonplaceholder.typicode.com/users', (data) => {
         console.log(data);
         jQuery(btnGetNames).on('click', function () {
             data.forEach(i => {
                 let userInfoNames = document.createElement('li');
+                let name = document.createElement('span');
                 let optionsNames = document.createElement('option');
+                let moreInfo = document.createElement('a');
+
                 userInfoNames.classList.add("table__item");
 
+                jQuery(moreInfo).attr('href', "#");
+
+                jQuery(moreInfo).text("More info...");
+                userInfoNames.append(name);
+                userInfoNames.append(moreInfo);
                 userName.append(userInfoNames);
                 selectNames.append(optionsNames);
 
-                jQuery(userInfoNames).html(i.name);
-                jQuery(optionsNames).html(i.name);
+                jQuery(name).text(i.name);
+                jQuery(optionsNames).text(i.name);
 
                 jQuery(this).off();
 
-                jQuery(userInfoNames).on('click', function () {
+                jQuery(moreInfo).on('click', function () {
                     popupOn();
                     blurOn();
+
+                    //get title
                     jQuery(userInfoTitle).text(i.name);
+
+                    //get email
+                    const email = document.createElement('li');
+                    email.classList.add("address__item");
+                    jQuery(userInfoMain).append(email);
+                    jQuery(email).html("Email: " + i.email);
+
+                    //get username
+                    const username = document.createElement('li');
+                    username.classList.add("address__item");
+                    jQuery(userInfoMain).append(username);
+                    jQuery(username).html("Username: " + i.username);
+
+                    //get phone
+                    const phone= document.createElement('li');
+                    phone.classList.add("address__item");
+                    jQuery(userInfoMain).append(phone);
+                    jQuery(phone).html("Phone: " + i.phone);
+
+                    //get website
+                    const website = document.createElement('li');
+                    website.classList.add("address__item");
+                    jQuery(userInfoMain).append(website);
+                    jQuery(website).html("Website: " + i.website);
+
+                    //get city
+                    const city = document.createElement('li');
+                    city.classList.add("address__item");
+                    jQuery(address).append(city);
+                    jQuery(city).html("City: " + i.address.city);
+
+                    //get street
+                    const street = document.createElement('li');
+                    street.classList.add("address__item");
+                    jQuery(address).append(street);
+                    jQuery(street).html("Street: " + i.address.street);
+
+                    //get suite
+                    const suite = document.createElement('li');
+                    suite.classList.add("address__item");
+                    jQuery(address).append(suite);
+                    jQuery(suite).html("Suite: " + i.address.suite);
+
+                    //get zipcode
+                    const zipcode = document.createElement('li');
+                    zipcode.classList.add("address__item");
+                    jQuery(address).append(zipcode);
+                    jQuery(zipcode).html("Zipcode: " + i.address.zipcode);
+
+                    //get company name
+                    const companyName = document.createElement('li');
+                    companyName.classList.add("address__item");
+                    jQuery(company).append(companyName);
+                    jQuery(companyName).html("Name: " + i.company.name);
+
+                    //get bs
+                    const bs = document.createElement('li');
+                    bs.classList.add("address__item");
+                    jQuery(company).append(bs);
+                    jQuery(bs).html("BS: " + i.company.bs);
+
+                    //get catch phrase
+                    const phrase = document.createElement('li');
+                    phrase.classList.add("address__item");
+                    jQuery(company).append(phrase);
+                    jQuery(phrase).html("Catch phrase: " + i.company.catchPhrase);
+
                     popupClose();
                 })
             });
         });
-
-        jQuery(btnGetUsernames).on('click', function () {
-            data.forEach(i => {
-                let userInfoUsernames = document.createElement('li');
-                userInfoUsernames.classList.add("table__item");
-                userUsername.append(userInfoUsernames);
-                jQuery(userInfoUsernames).html(i.username);
-                jQuery(this).off();
-            });
-        });
-
-        jQuery(btnGetEmail).on('click', function () {
-            data.forEach(i => {
-                let userInfoEmail = document.createElement('li');
-                userInfoEmail.classList.add("table__item");
-                userEmail.append(userInfoEmail);
-                jQuery(userInfoEmail).html(i.email);
-                jQuery(this).off();
-            });
-        });
-
     });
 });
 
